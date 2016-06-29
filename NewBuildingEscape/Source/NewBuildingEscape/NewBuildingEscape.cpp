198526293ed77bf9e7205dfeeee0e219385407b7
wwalker2 <wwalker2@xula.edu>
2016-06-28 19:58:26 -0500

    Commit 1

---
+++ NewBuildingEscape/Source/NewBuildingEscape/NewBuildingEscape.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NewBuildingEscape.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NewBuildingEscape, "NewBuildingEscape" );
---
+++ NewBuildingEscape/Source/NewBuildingEscape/NewBuildingEscapeGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NewBuildingEscape.h"
+#include "NewBuildingEscapeGameMode.h"
+
+
+
+
