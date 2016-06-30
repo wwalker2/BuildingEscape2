9d8ce69649197ab7383460e2939e030117fc3fc7
wwalker2 <wwalker2@xula.edu>
2016-06-30 14:23:18 -0500

    Commit 2

---
+++ NewBuildingEscape/Source/NewBuildingEscape/OpenDoor.cpp
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NewBuildingEscape.h"
+#include "OpenDoor.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	Owner = GetOwner();
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing pressure plate"), *GetOwner()->GetName());
+	}
+
+}
+
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//if (GetTotalMassOfActorsOnPlate() > TriggerMass) {
+	//	OnOpen.Broadcast();
+	//}
+
+	//else{
+	//	OnClose.Broadcast();
+	//}
+
+	if (ItemOnPlate1() == true) {
+		OnOpen.Broadcast();
+	}
+
+	else {
+		OnClose.Broadcast();
+	}
+}
+
+float UOpenDoor::GetTotalMassOfActorsOnPlate()
+{
+	float TotalMass = 0.f;
+
+	TArray<AActor*> OverlappingActors;
+	if (!PressurePlate) { return TotalMass; }
+	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
+
+	for (const auto& Actor : OverlappingActors)
+	{
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor->GetName());
+	}
+
+	return TotalMass;
+}
+
+TMap<FString, bool> UOpenDoor::MakeAList()
+{
+	TMap<FString, bool> CheckList;
+	CheckList.Add("SM_TableRound_4", false);
+	return CheckList;
+}
+
+bool UOpenDoor::ItemOnPlate1()
+{
+
+	TArray<AActor*> OverlappingActors;
+	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
+
+	for (int32 i = 0; i < OverlappingActors.Num(); i++)
+	{
+		if (OverlappingActors[0]->GetName().Equals(Key1->GetName())) {
+			Unlocked1 = true;
+			UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Key1->GetName());
+		}
+
+		//if (OverlappingActors[0]->GetName().Equals(Key2->GetName())) {
+		//	Unlocked2 = true;
+		//	UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Key2->GetName());
+		//}
+	}
+	//if (Unlocked1 == true && Unlocked2 == true) {
+	//	result = true;
+	//}
+	return Unlocked1;
+}
+
+bool UOpenDoor::ItemOnPlate2()
+{
+	TArray<AActor*> OverlappingActors;
+	PressurePlate2->GetOverlappingActors(OUT OverlappingActors);
+
+	for (int32 i = 0; i < OverlappingActors.Num(); i++)
+	{
+		if (OverlappingActors[0]->GetName().Equals(Key1->GetName())) {
+			Unlocked2 = true;
+			UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate 2"), *Key1->GetName());
+		}
+	}
+	return Unlocked2;
+}
+
---
+++ NewBuildingEscape/Source/NewBuildingEscape/OpenDoor.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NEWBUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnClose;
+
+private:
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate2 = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Keys")
+		AActor* Key1 = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Keys")
+		AActor* Key2 = nullptr;
+
+	AActor* Owner = nullptr;
+
+	float GetTotalMassOfActorsOnPlate();
+
+	UPROPERTY(EditAnywhere)
+		float TriggerMass = 30.f;
+
+	TMap<FString, bool> MakeAList();
+
+	bool ItemOnPlate1();
+
+	bool ItemOnPlate2();
+
+	bool Unlocked1 = false;
+	bool Unlocked2 = false;
+	
+};
